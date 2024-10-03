e6aa81dd475e31b890369455548489a3231a65b7
Aleksander005 <dmitrijdavydov853@gmail.com>
2024-10-03 23:20:49 +0300

    Add files via upload

--- module_1_3_Urban_University_Aleksandr005.py
+++ module_1_3_Urban_University_Aleksandr005.py
@@ -5,3 +5,11 @@ print (age)
 new_age = 'New age: 38'
 print (new_age)
 print ('Is student: True')
+
+first_name = 'Курс Python,' # Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+
+
+DZ_count = 1.5
+time_count = 12
+print (first_name, 'всего задач:' ,time_count, ',затрачено часов:',DZ_count, 'cреднее время выполнения',DZ_count/time_count)
+# не понял как поставить запятую без пробела после цифр и точку в конце предожения
