969dff44df3263a53bf465b594de93d39c5ea13c
Aleksander005 <dmitrijdavydov853@gmail.com>
2024-11-08 02:32:50 +0200

    Add files via upload

--- module_1_3_Urban_University_Aleksandr005.py
+++ module_1_3_Urban_University_Aleksandr005.py
@@ -1,24 +1,8 @@
-from tkinter.scrolledtext import example
-
+# модуль 1_3
 name = 'Name: Aleksandr'
-print (name)
+print(name)
 age = 'Age: 37'
-print (age)
+print(age)
 new_age = 'New age: 38'
-print (new_age)
-print ('Is student: True')
-
-first_name = 'Курс Python,' # Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
-
-
-DZ_count = 1.5
-time_count = 12
-print (first_name, 'всего задач:' ,time_count,',' 'затрачено часов:',DZ_count, 'cреднее время выполнения',DZ_count/time_count)
-# не понял как поставить запятую без пробела после цифр 12 и 1.5 и точку в конце предожения
-
-example = 'Aleksandr'
-print (example [:1]) #первый символ строки
-print (example [-1]) # последний символ строки
-print (example [4:10])# вторая половина строки не понимаю как посчитать количество символов в строке для установления чет нечет
-print (example [::-1]) # строка наоборот
-print (example [::2]) # каждый второй символ
+print(new_age)
+print('Is student: True')
